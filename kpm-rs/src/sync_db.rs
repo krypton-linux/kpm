ffe4530ce6bb0625ae507b15892df6c52505f545
小笠原 楓人 <fucho.021000@gmail.com>
2025-03-06 10:58:33 +0900

    sync_db.rs の更新

--- kpm-rs/src/sync_db.rs
+++ kpm-rs/src/sync_db.rs
@@ -4,7 +4,7 @@ use tr::tr;
 
 pub fn sync_db(){
     let Ok(alpm) = Alpm::new("/", "/var/lib/pacman") else{
-        eprintln!("Failed to initialize ALPM");
+        eprintln!("{}", tr!("Failed to initialize ALPM"));
         std::process::exit(1);
     };
 
